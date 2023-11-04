89e52bcd58618b023268026f423976e764a6ee80
Jakub Swistak <jakub.swistak@gmail.com>
2023-11-04 15:39:23 +0100

    BronKerbosch WIP

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-vector<int> biggestCliqueBK;
-
 vector<vector<int>> readMatrix(int n) {
     vector<vector<int>> matrix;
     int value;
@@ -70,74 +68,6 @@ int findPivot(const set<int> &P, const set<int> &X, const vector<vector<int>> &a
     return pivot;
 }
 
-void BronKerbosch2(set<int> R, set<int> P, set<int> X, const vector<vector<int>> &G) {
-    if (P.empty() && X.empty()) {
-        if (biggestCliqueBK.size() < R.size()) {
-            biggestCliqueBK.clear();
-            for (auto v : R) {
-                biggestCliqueBK.push_back(v);
-            }
-        }
-        // for (auto v : R) {
-        //     cout << v << ' ';
-        // }
-        // cout << endl;
-        return;
-    }
-
-    // Choosing pivot (vertex with the most neighbours in P and X)
-    int u = findPivot(P, X, G);
-
-    // Neighbours of u
-    set<int> diff;
-    for (auto v : P) {
-        if (!edgeExists(u, v, G)) {
-            diff.insert(v);
-        }
-    }
-
-    for (auto v : diff) {
-        set<int> newR = R, newP, newX;
-        newR.insert(v);
-
-        for (auto w : P) {
-            if (edgeExists(v, w, G)) {
-                newP.insert(w);
-            }
-        }
-
-        for (auto w : X) {
-            if (edgeExists(v, w, G)) {
-                newX.insert(w);
-            }
-        }
-
-        BronKerbosch2(newR, newP, newX, G);
-        P.erase(v);
-        X.insert(v);
-    }
-}
-
-void BronKerbosch3(const vector<vector<int>> &G) {
-    set<int> P, R, X;
-
-    for (int i = 0; i < G.size(); ++i) {
-        P.insert(i);
-    }
-
-    for (int i = 0; i < G.size(); ++i) {
-        set<int> neighbors;
-        for (int j = 0; j < G[i].size(); ++j) {
-            if (G[i][j] == 1) {
-                neighbors.insert(j);
-            }
-        }
-
-        BronKerbosch2({i}, neighbors, X, G);
-        P.erase(i);
-        X.insert(i);
-    }
-}
 
 bool isClique(std::vector<std::vector<int>> &graph, std::vector<int> &nodes) {
     for (int i = 0; i < nodes.size(); ++i) {
@@ -189,6 +119,46 @@ std::vector<int> monteCarloClique(std::vector<std::vector<int>> &graph, int iter
     return std::sort(max_clique.begin(), max_clique.end()), max_clique;
 }
 
+set<int> intersection(set<int> x, set<int> y) {
+    set<int> result;
+    set_intersection(x.begin(), x.end(), y.begin(), y.end(), inserter(result, result.end()));
+    return result;
+}
+
+set<int> neighbors(int v, const vector<vector<int>> &adj) {
+    set<int> result;
+    for (int i = 0; i < adj.size(); ++i) {
+        if (adj[v][i]) {
+            result.insert(i);
+        }
+    }
+    return result;
+}
+
+vector<int> biggestCliqueBK;
+void BronKerbosch(set<int> &R, set<int> &P, set<int> &X, const vector<vector<int>> &graph) {
+
+    if (P.empty() && X.empty()) {
+        cout << "MAX CLIQUE: " << endl;
+        for (auto a : R) {
+            cout << a << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int v : P) {
+        set<int> N = neighbors(v, graph);
+        set<int> Rv = R;
+        Rv.insert(v);
+        set<int> Pv = intersection(P, N);
+        set<int> Xv = intersection(X, N);
+        BronKerbosch(Rv, Pv, Xv, graph);
+
+        P.erase(v);
+        X.insert(v);
+    }
+}
 int main() {
     int n;
     cin >> n;
@@ -200,19 +170,29 @@ int main() {
     // 1. Find the biggest number of edges and replace it with eadge otherwise skip.
     // 2. If there is more edges between the same vertices we will replace multiple egdes with one.
 
-    vector<vector<int>> graph = reduceAllValuesToOne(matrix);
-    BronKerbosch3(graph); // exponential
+    vector<vector<int>> graph = reduceAllValuesToOne(matrix, 1);
 
-    cout << "Finished BronKerbosch3:\n";
+
+    cout << "BronKerbosch:\n";
+
+    set<int> R, P, X;
+    for (int i = 0; i < graph.size(); ++i) {
+        P.insert(i);
+    }
+    cout << "Running\n";
+    BronKerbosch(R, P, X, graph);
+    cout << "Not running\n";
+
     std::sort(biggestCliqueBK.begin(), biggestCliqueBK.end());
     for (auto v : biggestCliqueBK) {
         cout << v << " ";
     }
-    cout << endl;
 
     // Finding largest clique in polynomial time - Monte carlo aproximation
-    vector<int> largest_clique = monteCarloClique(graph, approximateIterations(graph));
+    vector<int> largest_clique = monteCarloClique(graph, 500000 * approximateIterations(graph));
+    // 0 2 8 13 16 17 21 25 26 31 34 36 39
     std::cout << "Largest clique Monte Carlo:\n";
+    cout << "Size: " << largest_clique.size() << endl;
     for (int x : largest_clique) {
         std::cout << x << " ";
     }
